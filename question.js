917ba545fbec963374bae79a88272b5a544568bb
Gobinda Das <gobinda993720@gmail.com>
2023-11-23 10:18:17 +0530

    add basic files

---
+++ question.js
@@ -0,0 +1,58 @@
+let date = new Date();
+let time = date.getHours();
+let name = prompt("enter your name?");
+let greeting = document.querySelector('h3');
+console.log(time);
+
+if (time < 12 && time > 6) {
+    greeting.innerText = "Good Morning " + name;
+} else if (time < 16) {
+    greeting.innerText = "Good After-noon " + name;
+} else if (time < 18) {
+    greeting.innerText = "Good Evening " +  name;
+} else {
+    greeting.innerText = "Good night " + name;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+let clickCount = localStorage.getItem('No of clicks') || 0;
+let button = document.querySelector('#click_btn');
+let restart = document.querySelector('#restart');
+
+// Function to change the style when clicked
+let clickFunction = ()=> {
+    button.innerText = `${clickCount} click`;
+    if (clickCount%2 == 0){
+        button.style.backgroundColor = 'lightseagreen';
+    } else {
+        button.style.backgroundColor = '#dd114e';
+    }
+    localStorage.setItem('No of clicks', clickCount);
+}
+
+
+// Giving the style before pressing the button
+clickFunction();
+
+// Changing the style according the no of presses
+button.addEventListener('click', ()=> {
+    clickCount++;
+    clickFunction();
+});
+
+
+// Reseting the value of clicks using restart button
+restart.addEventListener('click', ()=> {
+    clickCount = 0;
+    clickFunction();
+})
